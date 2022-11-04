71f977eacfb58e39287ed4b42c5c006518a39d1f
Angela Gonzalez <73128429+soyAngela@users.noreply.github.com>
2022-11-04 18:46:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -7,13 +7,7 @@ cache = redis.Redis(host='redis', port=6379)
 def get_hit_count():
     retries = 5
     while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+        return cache.incr('hits')
             
 @app.route('/')
 def hello():
